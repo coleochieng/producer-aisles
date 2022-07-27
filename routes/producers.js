76812e0c5049d66a325006c620b3b699bd64fdd9
coleochieng <nicole.ochieng98@gmail.com>
2022-07-27 17:11:10 -0500

    add functions to export on producers routes

---
+++ routes/producers.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+const producersCtrl = require('../controllers/producers');
+const isLoggedIn = require('../config/auth');
+
+// All routes "start with" /producer (from server.js)
+
+router.get('/', producersCtrl.index);
+// GET /producers/new (new functionality)
+router.get('/new', isLoggedIn, producersCtrl.new);
+// GET /producers/:id (show functionality)
+router.get('/:id', producersCtrl.show);
+// POST /producers (create functionality)
+router.post('/', isLoggedIn, producersCtrl.create);
+
+module.exports = router;
