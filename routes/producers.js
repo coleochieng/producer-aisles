e47b5787e31486696d1fd6c0309b0335f86658e0
coleochieng <nicole.ochieng98@gmail.com>
2022-07-29 06:34:34 -0500

    update producer routes

--- routes/producers.js
+++ routes/producers.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
-const moviesCtrl = require('../controllers/movies');
+const moviesCtrl = require('../controllers/producers');
 const isLoggedIn = require('../config/auth');
 
 // All routes "start with" /movies (from server.js)
 
 // GET /movies (index functionality)
-router.get('/', moviesCtrl.index);
+router.get('/', producersCtrl.index);
 // GET /movies/new (new functionality)
-router.get('/new', isLoggedIn, moviesCtrl.new);
+router.get('/new', isLoggedIn, producersCtrl.new);
 // GET /movies/:id (show functionality)
-router.get('/:id', moviesCtrl.show);
+router.get('/:id', producersCtrl.show);
 // POST /movies (create functionality)
-router.post('/', isLoggedIn, moviesCtrl.create);
+router.post('/', isLoggedIn, producersCtrl.create);
 
 module.exports = router;
